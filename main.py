fc702499f0feece04ac3ee9c70838eb8d5dc7ba8
Willy Cass <1844021+wcass77@users.noreply.github.com>
2025-04-24 15:27:44 -0400

    Implement player controls and game loop; add CircleShape base class

--- main.py
+++ main.py
@@ -8,19 +8,30 @@ from constants import (
     SCREEN_HEIGHT,
     SCREEN_WIDTH,
 )
+from player import Player
 
 
 def main() -> None:
     print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    # Game Loop
     while True:
+        # Check for events
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        # Update Game state
+        player.update(dt)
+
+        # Draw screen
         screen.fill(color=(0, 0, 0))
+        player.draw(screen)
+        dt = clock.tick(60)
         pygame.display.flip()
 
 
